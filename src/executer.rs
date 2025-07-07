870afc62a2302a03db63686075a92b4e53f5f2e2
Rov_Waff <2710182206@qq.com>
2025-07-07 16:16:33 +0800

    add:实现version,time,exit指令

--- src/executer.rs
+++ src/executer.rs
@@ -1,17 +1,26 @@
+use chrono::Local;
+
 use crate::command::COMMANDS;
 
-pub struct Executer{
-    command_type:COMMANDS,
-    command:String
+const VERSION: &str = env!("CARGO_PKG_VERSION");
+pub struct Executer {
+    command_type: COMMANDS,
+    command: String,
 }
 
-impl Executer{
-    pub fn exec(&self){
-        match self.command_type{
+impl Executer {
+    pub fn exec(&self) {
+        match self.command_type {
             COMMANDS::PRINT => todo!(),
-            COMMANDS::VERSION => todo!(),
-            COMMANDS::TIME => todo!(),
-            COMMANDS::EXIT => todo!(),
+            COMMANDS::VERSION => {
+                println!("当前版本:{}", VERSION)
+            }
+            COMMANDS::TIME => {
+                println!("当前时间:{}",Local::now().format("%Y-%m-%d %H:%M:%S"))
+            },
+            COMMANDS::EXIT => {
+                std::process::exit(0);
+            },
         }
     }
 }
