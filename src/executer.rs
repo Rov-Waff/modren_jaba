f30372e1943f8168cdf3e00d48813a2bca94f303
Rov_Waff <2710182206@qq.com>
2025-07-07 18:38:27 +0800

    add:完善ejz?指令

--- src/executer.rs
+++ src/executer.rs
@@ -12,30 +12,41 @@ impl Executer {
     pub fn exec(&self) {
         match self.command_type {
             COMMANDS::PRINT => {
-                let mut _status=false;
-                for i in self.command.chars(){
-                    if i=='?'&&_status==false{
-                        _status=true;
+                let mut _status = false;
+                for i in self.command.chars() {
+                    if i == '?' && _status == false {
+                        _status = true;
                         continue;
                     }
-                    if _status{
-                        print!("{}",i);
+                    if _status {
+                        print!("{}", i);
                     }
                 }
                 print!("\n");
-            },
+            }
             COMMANDS::VERSION => {
                 println!("当前版本:{}", VERSION)
             }
             COMMANDS::TIME => {
-                println!("当前时间:{}",Local::now().format("%Y-%m-%d %H:%M:%S"))
-            },
+                println!("当前时间:{}", Local::now().format("%Y-%m-%d %H:%M:%S"))
+            }
             COMMANDS::EXIT => {
                 std::process::exit(0);
-            },
+            }
+            COMMANDS::EJZ => {
+                let mut res = String::new();
+                for i in self.command[4..].chars() {
+                    res.push_str(&format!("{:032b}", i as u32));
+                    res.push_str(" ")
+                }
+                println!("{}", res);
+            }
         }
     }
-    pub fn new(_command_type:COMMANDS,_command:&String)->Executer{
-        Executer{command_type:_command_type,command:_command.to_string()}
+    pub fn new(_command_type: COMMANDS, _command: &String) -> Executer {
+        Executer {
+            command_type: _command_type,
+            command: _command.to_string(),
+        }
     }
 }
