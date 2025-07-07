0a63357e7205fc74836497e47db2eb49b91802a2
Rov_Waff <2710182206@qq.com>
2025-07-07 10:31:30 +0800

    add:输出版本信息

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+const VERSION:&str=env!("CARGO_PKG_VERSION");
+
 fn main() {
-    println!("Hello, world!");
+    println!(
+        "+-----------------+\n|Jaba 张浩扬与你同在\n|jvav 和 jaba作者:张浩扬\n|jvav改造者:Reiz\n|当前版本:{}\n|jaba改造者:xiaole6324\n|输入help来获取帮助\n+-----------------+
+",VERSION
+    );
+    
 }
