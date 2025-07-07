1a35b9f176ae6dd32acc5d5f73b72f4e3a88c0c4
Rov_Waff <2710182206@qq.com>
2025-07-07 15:47:50 +0800

    add:加一堆todo!

--- src/executer.rs
+++ src/executer.rs
@@ -4,3 +4,14 @@ pub struct Executer{
     command_type:COMMANDS,
     command:String
 }
+
+impl Executer{
+    pub fn exec(&self){
+        match self.command_type{
+            COMMANDS::PRINT => todo!(),
+            COMMANDS::VERSION => todo!(),
+            COMMANDS::TIME => todo!(),
+            COMMANDS::EXIT => todo!(),
+        }
+    }
+}
