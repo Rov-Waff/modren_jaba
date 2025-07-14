ccc10efedad18ed128ab8a3112db51793381a193
Rov_Waff <2710182206@qq.com>
2025-07-14 17:24:04 +0800

    fix:改版本

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ const VERSION: &str = env!("CARGO_PKG_VERSION");
 
 fn main() {
     println!(
-        "+-----------------+\n|Jaba 张浩扬与你同在\n|jvav 和 jaba作者:张浩扬\n|jvav改造者:Reiz\n|当前版本:{}\n|jaba改造者:xiaole6324\n|输入help来获取帮助\n+-----------------+
+        "+-----------------+\n|Jaba 张浩扬与你同在\n|jvav 和 jaba作者:张浩扬\n|jvav改造者:Reiz\n|当前版本:{}\n|jaba改造者:xiaole233\n|输入help来获取帮助\n+-----------------+
 ",VERSION
     );
     loop {
